b52c03ac61a3108f4b8a43e0bf13a15fc6c7b588
tri wahyudi <mesirah6@gmail.com>
2023-06-28 23:56:11 +0700

    add: menu list

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -25,81 +25,88 @@
 
 <div class="row bg-white border-4 p-3">
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+            <i class="fa-solid fa-money-bill-transfer" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">Transfer</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+            <i class="fa-solid fa-money-bill-wave" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">Tarik Tunai</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+            <i class="fa-solid fa-hand-holding-dollar" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">Setor Tunai</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+            <i class="fa-regular fa-credit-card" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">Bayar Cicilan</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+            <i class="fa-solid fa-file-shield" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">Asuransi</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+            <i class="fa-solid fa-mobile-screen-button" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">Pulsa</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+            <i class="fa-solid fa-bolt-lightning" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">PLN</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+                <h5 class="card-title pt-2 mb-0">Transfer</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+                <h5 class="card-title pt-2 mb-0">Transfer</h5>
             </div>
         </div>
     </div>
 
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100">
-            <div class="card-body">
-                <h5 class="card-title text-center">Transfer</h5>
+        <div class="card h-100 border-0">
+            <div class="card-body text-center">
+                <h5 class="card-title pt-2 mb-0">Transfer</h5>
             </div>
         </div>
     </div>
