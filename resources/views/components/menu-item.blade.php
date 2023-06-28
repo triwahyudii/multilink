2352d3a1f35b764d061f066bf76ce2895507527e
tri wahyudi <mesirah6@gmail.com>
2023-06-28 23:09:22 +0700

    add: icon

---
+++ resources/views/components/menu-item.blade.php
@@ -0,0 +1,106 @@
+<!-- <div class="bg-white">
+    <div class="grid text-center">
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2"><i class="fa-solid fa-money-bill-transfer" style="color: #1f66e0;"></i>
+            <p class="h6 text-bold">Bill</p>
+        </div>
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+        <div class="g-col-4 col-sm-6 col-md-4 col-lg-3-6 g-col-4 col-sm-6 col-md-4 col-lg-3-md-4 g-col-4 col-sm-6 col-md-4 col-lg-3-lg-3 border-2">.g-col-4 col-sm-6 col-md-4 col-lg-3-4 .g-col-4 col-sm-6 col-md-4 col-lg-3-md-4</div>
+        {{$slot}}
+    </div>
+</div>
+
+
+<i class="fa-solid fa-money-bill-transfer" style="color: #1f66e0;"></i> -->
+
+<div class="row bg-white border-4 p-3">
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card h-100">
+            <div class="card-body">
+                <h5 class="card-title text-center">Transfer</h5>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,12 +9,7 @@
 
     </x-slot>
 
-    <div class="py-12 mt-0 pt-1">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+    <!-- Menu Item or List Menu -->
+    <x-menu-item />
+    
 </x-app-layout>
