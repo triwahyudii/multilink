7e9270563b52167d0f23f6511ace703ed7ee90c7
tri wahyudi <mesirah6@gmail.com>
2023-06-28 11:49:38 +0700

    add: icon

--- resources/views/components/head-item.blade.php
+++
@@ -1,6 +0,0 @@
-
-<div class="flex justify-end">
-    <i class="fas fa-shopping-cart mr-1"></i>
-    <i class="fas fa-bell mr-4"></i>
-    {{$slot}}
-</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,36 @@
+<!-- <nav class="navbar">
+    <div class="container">
+        <div>
+            <p class="bolder">Tagihan</p>
+        </div>
+        <div>
+            <p class="bolder">Saldo</p>
+        </div>
+        <div class="d-flex justify-content-end mt-5">
+            <div class="mr-2">
+                <i class="fas fa-shopping-cart"></i>
+            </div>
+            <div class="ml-4 sm:ml-6">
+                <i class="fas fa-bell"></i>
+            </div>
+            {{$slot}}
+        </div>
+    </div>
+</nav> -->
+
+<nav class="navbar mt-5">
+    <div class="container-fluid">
+        <div>
+            <span class="h5">Tagihan</span>
+        </div>
+        <div>
+            <span class="h5">Saldo</span>
+        </div>
+        <div class="ml-4">
+            <i class="fas fa-shopping-cart"></i>
+        </div>
+        <div>
+            <i class="fas fa-bell"></i>
+        </div>
+    </div>
+</nav>
