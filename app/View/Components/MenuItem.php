dbf348e6e8a853b78efff2d41b3d68a3421bb7b7
tri wahyudi <mesirah6@gmail.com>
2023-06-30 00:28:11 +0700

    add: components

---
+++ app/View/Components/MenuItem.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class MenuItem extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $menu = [
+            [
+                'title' => 'Transfer',
+                'icon' => 'fa-solid fa-money-bill-transfer',
+            ],
+            [
+                'title' => 'Tarik Tunai',
+                'icon' => 'fa-solid fa-money-bill-wave'
+            ],
+            [
+                'title' => 'Setor Tunai',
+                'icon' => 'fa-solid fa-hand-holding-dollar'
+            ],
+            [
+                'title' => 'Bayar Cicilan',
+                'icon' => 'fa-regular fa-credit-card'
+            ],
+            [
+                'title' => 'Asuransi',
+                'icon' => 'fa-solid fa-file-shield'
+            ],
+            [
+                'title' => 'Pengajuan Pinjaman',
+                'icon' => 'fa-solid fa-building-columns'
+            ],
+            [
+                'title' => 'Pulsa',
+                'icon' => 'fa-solid fa-mobile-screen-button'
+            ],
+            [
+                'title' => 'PLN',
+                'icon' => 'fa-solid fa-bolt-lightning'
+            ],
+            [
+                'title' => 'Top Up',
+                'icon' => 'fa-brands fa-steam'
+            ],
+            [
+                'title' => 'Bahan Dapur',
+                'icon' => 'fa-solid fa-kitchen-set'
+            ],
+            [
+                'title' => 'Sayur Saji',
+                'icon' => 'fa-solid fa-bag-shopping'
+            ],
+        ];
+        return view('components.menu-item', compact('menu'));
+    }
+}
--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,102 +1,14 @@
 <div class="row bg-white border-4 p-3">
+
+    @foreach($menu as $item)
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
         <div class="card h-100 border-0">
             <div class="card-body text-center">
-                <i class="fa-solid fa-money-bill-transfer" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Transfer</h5>
+                <i class="{{ $item['icon'] }}" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                <h5 class="card-title pt-2 mb-0">{{ $item['title'] }}</h5>
             </div>
         </div>
     </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-money-bill-wave" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Tarik Tunai</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-hand-holding-dollar" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Setor Tunai</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-regular fa-credit-card" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Bayar Cicilan</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-file-shield" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Asuransi</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-building-columns" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px; pointer-events:none; filter:blur(3px);"></i>
-                <h5 class="card-title pt-2 mb-0">Pengajuan Pinjaman</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-mobile-screen-button" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Pulsa</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-bolt-lightning" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">PLN</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-brands fa-steam" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Top Up</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-kitchen-set" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Bahan Dapur</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="fa-solid fa-bag-shopping" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">Sayur Saji</h5>
-            </div>
-        </div>
-    </div>
-
+    @endforeach
 
 </div>
