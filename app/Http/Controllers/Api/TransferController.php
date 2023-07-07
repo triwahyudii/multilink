653e46179eac916d8120bb3c3ce8285144df8381
tri wahyudi <mesirah6@gmail.com>
2023-07-07 21:38:35 +0700

    add: controller

---
+++ app/Http/Controllers/Api/TransferController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transfer;
+use Illuminate\Http\Request;
+
+class TransferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Transfer::orderBy('nama')->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'Data berhasil diambil',
+            'data' => $data
+        ], 200); //response 200
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
