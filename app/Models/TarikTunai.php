a21053b9b7d8096274d752e3af30002869c2d3e2
tri wahyudi <mesirah6@gmail.com>
2023-07-08 22:18:15 +0700

    api tarik tunai

---
+++ app/Models/TarikTunai.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TarikTunai extends Model
+{
+    use HasFactory;
+
+    protected $table = "tarik_tunais";
+    protected $fillable = [
+        'nama',
+        'nomor_rekening',
+        'jumlah'
+    ];
+}
---
+++ database/migrations/2023_07_08_145600_create_tarik_tunais_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tarik_tunais', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 200);
+            $table->bigInteger('nomor_rekening')->length(17);
+            $table->bigInteger('jumlah')->length(10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tarik_tunais');
+    }
+};
