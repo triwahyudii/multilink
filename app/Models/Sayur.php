e8c68e84964d8ec274e2328ef307c10d35d7df50
tri wahyudi <mesirah6@gmail.com>
2023-07-18 22:00:33 +0700

    add: model sayur

---
+++ app/Models/Sayur.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sayur extends Model
+{
+    use HasFactory;
+
+    protected $table = 'sayurs';
+    protected $fillable = [
+        'nama',
+        'harga',
+        'deskripsi',
+        'image'
+    ];
+}
