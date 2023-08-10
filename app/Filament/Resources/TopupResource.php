8adb456bfcc6e102555392e61a59a2d0bf7c6baf
tri wahyudi <mesirah6@gmail.com>
2023-08-10 23:50:40 +0700

    add: data admin topup

--- app/Filament/Resources/TopupResource.php
+++ app/Filament/Resources/TopupResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\TopupResource\Pages;
 use App\Filament\Resources\TopupResource\RelationManagers;
 use App\Models\Topup;
 use Filament\Forms;
+use Filament\Forms\Components\Card;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
@@ -23,13 +24,32 @@ class TopupResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('nama')
-                    ->required()
-                    ->maxLength(100),
-                Forms\Components\TextInput::make('nomor_id')
-                    ->required(),
-                Forms\Components\TextInput::make('jumlah')
-                    ->required(),
+                Card::make()->schema([
+                    Forms\Components\Select::make('nama')
+                        ->label('Game')
+                        ->options([
+                            'DOMINO HIGGS' => 'DOMINO HIGGS',
+                            'MOBILE LEGEND' => 'MOBILE LEGEND',
+                            'AOV' => 'AOV',
+                            'FREE FIRE' => 'FREE FIRE',
+                            'CALL OF DUTY' => 'CALL OF DUTY',
+                            'PUBG' => 'PUBG',
+                        ])->required(),
+                    Forms\Components\TextInput::make('nomor_id')
+                        ->integer(30)
+                        ->required(),
+                    Forms\Components\Select::make('jumlah')
+                        ->integer(20)
+                        ->options([
+                            '10' => '10 Item',
+                            '50' => '50 Item',
+                            '100' => '100 Item',
+                            '200' => '200 Item',
+                            '500' => '500 Item',
+                            '1000' => '1000 Item',
+                        ])
+                        ->required(),
+                ])
             ]);
     }
 
@@ -37,38 +57,39 @@ class TopupResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('nama'),
-                Tables\Columns\TextColumn::make('nomor_id'),
-                Tables\Columns\TextColumn::make('jumlah'),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime(),
-                Tables\Columns\TextColumn::make('updated_at')
+                Tables\Columns\TextColumn::make('nama')->sortable()->searchable()->label('Game'),
+                Tables\Columns\TextColumn::make('nomor_id')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('jumlah')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->label('Entered')
                     ->dateTime(),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
             'index' => Pages\ListTopups::route('/'),
             'create' => Pages\CreateTopup::route('/create'),
             'edit' => Pages\EditTopup::route('/{record}/edit'),
+            'view' => Pages\ViewTopup::route('/{record}'),
         ];
-    }    
+    }
 }
