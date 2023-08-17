631edf022071bdad81d2263ee02b6017b6ba0b12
tri wahyudi <mesirah6@gmail.com>
2023-08-17 23:16:21 +0700

    change icon menu

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -36,7 +36,7 @@
         <a href="http://127.0.0.1:8000/setor-tunai">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-solid fa-hand-holding-dollar" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Setor-tunai.png') }}" alt="Setor-tunai" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">Setor Tunai</h5>
                 </div>
             </div>
@@ -47,7 +47,7 @@
         <a href="http://127.0.0.1:8000/bayar-cicilan">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-regular fa-credit-card" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Bayar.png') }}" alt="Bayar" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">Bayar Cicilan</h5>
                 </div>
             </div>
@@ -58,7 +58,7 @@
         <a href="http://127.0.0.1:8000/asuransi">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-solid fa-file-shield" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Asuransi.png') }}" alt="Asuransi" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">Asuransi</h5>
                 </div>
             </div>
@@ -68,7 +68,7 @@
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
         <div class="card h-100 border-0">
             <div class="card-body text-center">
-                <i class="fa-solid fa-building-columns" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px; pointer-events:none; filter:blur(3px);"></i>
+                <img src="{{ asset('assets/images/Bank.png') }}" alt="Bank" style=" display:flex; justify-content:center; align-items:center; font-size:40px;  filter:blur(3px);">
                 <h5 class="card-title pt-2 mb-0">Pengajuan Pinjaman</h5>
             </div>
         </div>
@@ -78,7 +78,7 @@
         <a href="http://127.0.0.1:8000/pulsa">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-solid fa-mobile-screen-button" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Pulsa.png') }}" alt="Pulsa" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">Pulsa</h5>
                 </div>
             </div>
@@ -89,7 +89,7 @@
         <a href="http://127.0.0.1:8000/pln">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-solid fa-bolt-lightning" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Pln.png') }}" alt="Pln" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">PLN</h5>
                 </div>
             </div>
@@ -100,7 +100,7 @@
         <a href="http://127.0.0.1:8000/topup">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-brands fa-steam" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Topup.png') }}" alt="Topup" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">Top Up</h5>
                 </div>
             </div>
@@ -111,7 +111,7 @@
         <a href="http://127.0.0.1:8000/dapur">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-solid fa-kitchen-set" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Dapur.png') }}" alt="Dapur" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">Bahan Dapur</h5>
                 </div>
             </div>
@@ -122,7 +122,7 @@
         <a href="http://127.0.0.1:8000/sayur">
             <div class="card h-100 border-0">
                 <div class="card-body text-center">
-                    <i class="fa-solid fa-bag-shopping" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
+                    <img src="{{ asset('assets/images/Sayur.png') }}" alt="Sayur" style=" display:flex; justify-content:center; align-items:center; font-size:40px;">
                     <h5 class="card-title pt-2 mb-0">Sayur Saji</h5>
                 </div>
             </div>
