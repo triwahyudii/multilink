1bcbb12fa0668f965dd7a4407825e6084539259b
tri wahyudi <mesirah6@gmail.com>
2023-09-08 23:28:25 +0700

    function payment

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,19 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'user_id', // Add other necessary attributes
-    ];
-
-    public function dapurProducts()
-    {
-        return $this->belongsToMany(Dapur::class, 'order_dapur')
-            ->withPivot('quantity') // You can add more pivot columns if needed
-            ->withTimestamps();
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $guarded = [];
 }
