5c9cf3ab8e3bae9e6401bd9fe5f227ba43a2e4e6
tri wahyudi <mesirah6@gmail.com>
2023-09-29 15:59:58 +0700

    bug

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,5 @@
 <div class="bg-body-tertiary p-3 pb-4 mb-5">
     <div class="d-grid gap-2">
         <button class="btn btn-outline-primary bg-primary text-white mx-4 px-6" type="button"><a href="http://127.0.0.1:8000/riwayat">Riwayat</a></button>
-        <!-- <button class="btn btn-outline-primary mx-4 px-6" type="button">Pesanan</button> -->
     </div>
 </div>
