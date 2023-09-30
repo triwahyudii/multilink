dd6c60b0fe5003326a84fdbf6d34843002a770e6
tri wahyudi <mesirah6@gmail.com>
2023-09-30 10:52:34 +0700

    debugging

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,15 +1,4 @@
 <div class="row bg-white border-4 p-3 m-0 rounded-4">
-
-    <!-- @foreach($menu as $item)
-    <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
-        <div class="card h-100 border-0">
-            <div class="card-body text-center">
-                <i class="{{ $item['icon'] }}" style="color: #1f66e0; display:flex; justify-content:center; align-items:center; font-size:40px;"></i>
-                <h5 class="card-title pt-2 mb-0">{{ $item['title'] }}</h5>
-            </div>
-        </div>
-    </div>
-    @endforeach -->
     <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
         <a href="http://127.0.0.1:8000/transfer">
             <div class="card h-100 border-0">
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,22 +1,3 @@
-<!-- <nav class="navbar">
-    <div class="container">
-        <div>
-            <p class="bolder">Tagihan</p>
-        </div>
-        <div>
-            <p class="bolder">Saldo</p>
-        </div>
-        <div class="d-flex justify-content-end mt-5">
-            <div class="mr-2">
-                <i class="fas fa-shopping-cart"></i>
-            </div>
-            <div class="ml-4 sm:ml-6">
-                <i class="fas fa-bell"></i>
-            </div>
-            {{$slot}}
-        </div>
-    </div>
-</nav> -->
 
 <nav class="navbar mt-5 pb-0">
     <div class="container-fluid">
